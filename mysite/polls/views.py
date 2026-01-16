1af009d7cce9ea460c91924951dcd1d7ae86a8bb
git <Git@git>
2026-01-16 21:28:50 +0800

    1

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
     return HttpResponse("Hello, world. 816751b2 You're at the polls index.")
