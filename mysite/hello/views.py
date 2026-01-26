2a517f5d407fd6d173b250bd8cddd80b7bf52f8e
git <Git@git>
2026-01-27 00:23:21 +0800

    1

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -28,6 +28,9 @@ def sessfun(request) :
 
 
 def myview(request):
-    resp = HttpResponse('Hello World')
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits 
+    if num_visits > 4 : del(request.session['num_visits'])
+    resp = HttpResponse('view count='+str(num_visits))
     resp.set_cookie('dj4e_cookie', settings.USER_ID, max_age=1000)
     return resp
