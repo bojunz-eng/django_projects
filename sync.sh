5c73751d2d4840ee0b48a38c31b3a543f1aaa613
git <Git@git>
2026-03-20 23:47:08 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,5 +4,12 @@ cd django_projects
 
 git pull origin main
 
-cd mysite
+cd market
+
+mv -f config/settings_local.py config/settings.py
+
+python manage.py makemigrations
+python manage.py migrate
 python manage.py check
+
+
